d397fe824575839d55e8464000a7178e4132e6ab
Steeven <steevencg612@gmail.com>
2023-10-19 15:39:03 -0500

    Version 2

--- js/app.js
+++ js/app.js
@@ -42,6 +42,7 @@ const cargarTabla = () => {
         <td>${item.tipo}</td>
         <td>${item.tiempo}</td>
         <td>$${item.precio}</td>
+        <td>$${item.imagen}</td>
         <td>
         <div class="d-flex gap-2">
         <button class="btn btn-outline-warning" onclick="mostrarModal(${item.id})"><i class="fa fa-pencil" aria-hidden="true"></i></button>
